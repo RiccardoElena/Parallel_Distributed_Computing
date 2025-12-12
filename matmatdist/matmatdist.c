ff8cce2acfd78e7142a2948813bb309958f7df2e
43616F73 <genttneg3@gmail.com>
2025-12-12 14:59:22 +0100

    Fix. matmatdist. Need to be tested on cluster

--- matmatdist/matmatdist.c
+++ matmatdist/matmatdist.c
@@ -38,20 +38,20 @@ void matmatdist(MPI_Comm Gridcom,
 
 static int gcd(int a, int b)
 {
-  while (b != 0)
-  {
-    int temp = b;
+  int tmp;
+
+  while (b) {
+    tmp = b;
     b = a % b;
-    a = temp;
+    a = tmp;
   }
+
   return a;
 }
 
 static int lcm(int a, int b)
 {
-  if (a == 0 || b == 0)
-    return 0;
-  return (a / gcd(a, b)) * b;
+  return (a && b) ? 0 : (a / gcd(a, b)) * b;
 }
 
 void matmatikj(int lda, int ldb, int ldc,
@@ -59,16 +59,22 @@ void matmatikj(int lda, int ldb, int ldc,
                int N1, int N2, int N3)
 {
   int i, j, k;
-  int aik;
-  for (i = 0; i < N1; i++)
-  {
-    for (k = 0; k < N2; k++)
-    {
-      aik = A[i * lda + k];
-      for (j = 0; j < N3; j++)
-      {
-        // C[i * ldc + j] = C[i * ldc + j] + A[i * lda + k] * B[k * ldb + j];
-        C[i * ldc + j] += aik * B[k * ldb + j];
+  double aik;
+  const double* restrict rA = A;
+  const double* restrict rB = B;
+  double* restrict rC = C;
+  const double* Arow;
+  const double* Brow;
+  double* Crow;
+
+  for (i = 0; i < N1; ++i) {
+    Arow = rA + i * lda;
+    Crow = rC + i * ldc;
+    for (k = 0; k < N2; ++k) {
+      aik = Arow[k];
+      Brow = rB + k * ldb;
+      for (j = 0; j < N3; ++j) {
+        Crow[j] += aik * Brow[j];
       }
     }
   }
@@ -79,22 +85,28 @@ void matmatblock(int lda, int ldb, int ldc,
                  int N1, int N2, int N3,
                  int dbA, int dbB, int dbC)
 {
-  int ii_blocs = N1 / dbA + (N1 % dbA != 0);
-  int kk_blocs = N2 / dbB + (N2 % dbB != 0);
-  int jj_blocs = N3 / dbC + (N3 % dbC != 0);
-  int ii, jj, kk;
-  for (ii = 0; ii < ii_blocs; ii++)
-    for (jj = 0; jj < jj_blocs; jj++)
-      for (kk = 0; kk < kk_blocs; kk++)
-      {
+  int nbi, nbk, nbj;
+  int ii, kk, jj;
+  int ni, nk, nj;
+
+  nbi = (N1 + dbA - 1) / dbA;
+  nbk = (N2 + dbB - 1) / dbB;
+  nbj = (N3 + dbC - 1) / dbC;
+  
+  for (ii = 0; ii < nbi; ++ii) {
+    ni = (ii < nbi - 1) ? dbA : N1 - ii * dbA;
+    for (kk = 0; kk < nbk; ++kk) {
+      nk = (kk < nbk - 1) ? dbB : N2 - kk * dbB;
+      for (jj = 0; jj < nbj; ++jj) {
+        nj = (jj < nbj - 1) ? dbC : N3 - jj * dbC;
         matmatikj(lda, ldb, ldc,
-                  &A[(ii * dbA) * lda + (kk * dbB)],
-                  &B[(kk * dbB) * ldb + (jj * dbC)],
-                  &C[(ii * dbA) * ldc + (jj * dbC)],
-                  (ii != ii_blocs - 1) ? dbA : (N1 - ii * dbA),
-                  (kk != kk_blocs - 1) ? dbB : (N2 - kk * dbB),
-                  (jj != jj_blocs - 1) ? dbC : (N3 - jj * dbC));
+                  A + ii * dbA * lda + kk * dbB,
+                  B + kk * dbB * ldb + jj * dbC,
+                  C + ii * dbA * ldc + jj * dbC,
+                  ni, nk, nj);
       }
+    }
+  }
 }
 
 void matmatthread(int lda, int ldb, int ldc,
@@ -103,30 +115,27 @@ void matmatthread(int lda, int ldb, int ldc,
                   int dbA, int dbB, int dbC,
                   int NTROW, int NTCOL)
 {
-#pragma omp parallel num_threads(NTROW *NTCOL)
+#pragma omp parallel num_threads(NTROW * NTCOL)
   {
-    int thread_id = omp_get_thread_num();
-    int row_id = thread_id / NTCOL;
-    int col_id = thread_id % NTCOL;
+    int tid, row_id, col_id;
+    int row_start, col_start;
+    int local_N1, local_N3;
+    
+    tid = omp_get_thread_num();
+    row_id = tid / NTCOL;
+    col_id = tid % NTCOL;
 
-    int rows_per_thread = N1 / NTROW;
-    int cols_per_thread = N3 / NTCOL;
+    row_start = row_id * (N1 / NTROW);
+    col_start = col_id * (N3 / NTCOL);
 
-    int row_start = row_id * rows_per_thread;
-    int row_end = (row_id == NTROW - 1) ? N1 : row_start + rows_per_thread;
+    local_N1 = (row_id < NTROW -1) ? N1 / NTROW : N1 - row_start;
+    local_N3 = (col_id < NTCOL -1) ? N3 / NTCOL : N3 - col_start;
 
-    int col_start = col_id * cols_per_thread;
-    int col_end = (col_id == NTCOL - 1) ? N3 : col_start + cols_per_thread;
-
-    int local_N1 = row_end - row_start;
-    int local_N3 = col_end - col_start;
-
-    if (local_N1 > 0 && local_N3 > 0)
-    {
+    if (local_N1 > 0 && local_N3 > 0) {
       matmatblock(lda, ldb, ldc,
-                  &A[row_start * lda],
-                  &B[col_start],
-                  &C[row_start * ldc + col_start],
+                  A + row_start * lda,
+                  B + col_start,
+                  C + row_start * ldc + col_start,
                   local_N1, N2, local_N3,
                   dbA, dbB, dbC);
     }
@@ -141,60 +150,57 @@ void matmatdist(MPI_Comm Gridcom,
                 int NTROW, int NTCOL)
 {
   MPI_Comm Rowcom, Colcom;
-  int ndims = 2, dims[2], periods[2], coords[2];
-  int NProws, NPcol;
-  int N1local, N2local, N3local;
-  int directions[2] = {0, 1};
-  int K2;
-  int i, k, c, r;
+  int dims[2], periods[2], coords[2];
+  int remain[2];
+  int NProw, NPcol, K;
+  int N1loc, N2loc, N3loc;
+  int i, k, r, c;
   double *Acol, *Brow;
 
-  MPI_Cart_get(Gridcom, ndims, dims, periods, coords);
-
-  NProws = dims[0];
+  MPI_Cart_get(Gridcom, 2, dims, periods, coords);
+  NProw = dims[0];
   NPcol = dims[1];
+  K = lcm(NProw, NPcol);
 
-  {
-    int a = NPcol, b = NProws;
-    int temp;
-    while (b != 0)
-    {
-      temp = b;
-      b = a % b;
-      a = temp;
-    }
-    K2 = (a == 0 || b == 0) ? 0 : (NPcol / a) * NProws;
-  }
+  N1loc = N1 / NProw;
+  N2loc = N2 / K;
+  N3loc = N3 / NPcol;
 
-  N1local = N1 / NProws;
-  N2local = N2 / K2;
-  N3local = N3 / NPcol;
+  Acol = (double*) malloc(N1loc * N2loc * sizeof(double));
+  Brow = (double*) malloc(N2loc * N3loc * sizeof(double));
 
-  Acol = (double *)malloc(N1local * N2local * sizeof(double));
-  Brow = (double *)malloc(N2local * N3local * sizeof(double));
-  MPI_Cart_sub(Gridcom, directions, &Rowcom);
-  directions[0] = 1;
-  directions[1] = 0;
-  MPI_Cart_sub(Gridcom, directions, &Colcom);
+  remain[0] = 0;
+  remain[1] = 1;
 
-  for (k = 0; k < K2; ++k)
-  {
+  MPI_Cart_sub(Gridcom, remain, &Rowcom);
+
+  remain[0] = 1;
+  remain[1] = 0;
+
+  MPI_Cart_sub(Gridcom, remain, &Colcom);
+
+  for (k = 0; k < K; ++k) {
+    r = k % NProw;
     c = k % NPcol;
-    r = k % NProws;
 
-    if (coords[0] == r)
-      for (i = 0; i < N1local; i++)
-        memcpy(&Acol[i * N2local], &A[i * lda], N2local * sizeof(double));
+    if (coords[0] == r) {
+      for (i = 0; i < N1loc; ++i) {
+        memcpy(Acol + i * N2loc, A + i * lda, N2loc * sizeof(double));
+      }
+    }
 
-    if (coords[1] == c)
-      for (i = 0; i < N2local; i++)
-        memcpy(&Brow[i * N3local], &B[i * ldb], N3local * sizeof(double));
+    if (coords[1] == c) {
+      for (i = 0; i < N2loc; ++i) {
+        memcpy(Brow + i * N3loc, B + i * ldb, N3loc * sizeof(double));
+      }
+    }
 
-    MPI_Bcast(Acol, N1local * N2local, MPI_DOUBLE, r, Colcom);
-    MPI_Bcast(Brow, N2local * N3local, MPI_DOUBLE, c, Rowcom);
-    matmatthread(N2local, N3local, ldc,
+    MPI_Bcast(Acol, N1loc * N2loc, MPI_DOUBLE, r, Colcom);
+    MPI_Bcast(Brow, N2loc * N3loc, MPI_DOUBLE, c, Rowcom);
+
+    matmatthread(N2loc, N3loc, ldc,
                  Acol, Brow, C,
-                 N1local, N2local, N3local,
+                 N1loc, N2loc, N3loc,
                  dbA, dbB, dbC,
                  NTROW, NTCOL);
   }
